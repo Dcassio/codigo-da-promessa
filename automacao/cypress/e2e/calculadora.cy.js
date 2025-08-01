1d22f91336b72d71bfd6d72509604d738c6c3941
Douglas cássio <dodasso.jd@gmail.com>
2025-08-01 00:26:43 -0300

    feat: testes automatizados da calculadora com Cypress

--- automacao/cypress/e2e/calculadora.cy.js
+++ automacao/cypress/e2e/calculadora.cy.js
@@ -10,10 +10,11 @@ describe('Calculadora - Testes Básicos', () => {
     cy.get('#answer').should('have.text', '15')
   });
 
-  it('Divisão por zero deve mostrar erro ou resultado', () => {
+  it('Divisão por zero deve mostrar erro ou resultado igual a ele msm', () => {
     cy.get('#number1').type('10')
     cy.get('#number2').type('0')
-    cy.get('#calculate').click()
-    cy.get('#answer').should('exist') // Adapte conforme o comportamento do sistema
+    cy.get('#operacao').select('/');
+    cy.get('#calcular').click();
+    cy.get('#resultado').should('contain', 'Erro' || 'Infinito');
   });
 });
