0863cf11d2d41eb6306aa41ab8c20851996a66f0
Douglas cássio <dodasso.jd@gmail.com>
2025-08-10 01:11:24 -0300

    Correção

--- automacao/cypress/e2e/calculadora.cy.js
+++ automacao/cypress/e2e/calculadora.cy.js
@@ -13,8 +13,7 @@ describe('Calculadora - Testes Básicos', () => {
   it('Divisão por zero deve mostrar erro ou resultado igual a ele msm', () => {
     cy.get('#number1').type('10')
     cy.get('#number2').type('0')
-    cy.get('#operacao').select('/');
-    cy.get('#calcular').click();
-    cy.get('#resultado').should('contain', 'Erro' || 'Infinito');
+    cy.get('#calculate').click();
+    cy.get('#answer').should('have.text', '10');
   });
 });
