831bcfd163a80397446bf75d7d103bc0926e4dd4
Douglas cássio <dodasso.jd@gmail.com>
2025-08-10 01:09:49 -0300

    feat(automacao): add calculator

---
+++ automacao/cypress/e2e/calculator.cy.js
@@ -0,0 +1,12 @@
+import Calculator from '../pageObjects/calculatorPage.js'
+
+describe('Teste da Calculadora', () => {
+  it('Deve somar dois números', () => {
+    const calc = new Calculator()
+    calc.visit()
+    calc.enterFirstNumber(5)
+    calc.enterSecondNumber(3)
+    calc.clickAdd()
+    calc.verifyResult(8)
+  })
+})
